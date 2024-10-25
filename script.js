bdbdffce97a2603f158614b80ef85514be15e1e7
Noufiya <noufiya143@gmail.com>
2024-10-25 10:20:58 +0530

    construction web first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function()
+		{
+			$(".bg1").mouseover(function()
+			{
+				$(this).css("background-image", "url(https://okcredit-blog-images-prod.storage.googleapis.com/2021/02/construction1.jpg)");
+			});
+			$(".bg1").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg2").mouseover(function()
+			{
+				$(this).css("background-image", "url(https://www.hashmicro.com/blog/wp-content/uploads/2022/10/Tools-16.jpg)");
+			});
+			$(".bg2").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg3").mouseover(function()
+			{
+				$(this).css("background-image", "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXofgplvIvQdHDr3xAS17hKhFJzJ0n-moU8cx2TIbzSHy8HzKJMQCIbRrGogkR1YkNcM0&usqp=CAU)");
+			});
+			$(".bg3").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg4").mouseover(function()
+			{
+				$(this).css("background-image", "url(https://www.hashmicro.com/blog/wp-content/uploads/2022/10/Tools-16.jpg)");
+			});
+			$(".bg4").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg5").mouseover(function()
+			{
+				$(this).css("background-image", "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR0gjn-LWpQ1lHVv39nDqSicYYCtBYUESlapVOI6tkURbVJo67Wc4JWDINFfE84Hvwnfg&usqp=CAU)");
+			});
+			$(".bg5").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg6").mouseover(function()
+			{
+				$(this).css("background-image", "url(https://i0.wp.com/theconstructor.org/wp-content/uploads/2021/08/shutterstock_257689849.jpg?fit=1000%2C717&ssl=1)");
+			});
+			$(".bg6").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg7").mouseover(function()
+			{
+				$(this).css("background-image", "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR0gjn-LWpQ1lHVv39nDqSicYYCtBYUESlapVOI6tkURbVJo67Wc4JWDINFfE84Hvwnfg&usqp=CAU)");
+			});
+			$(".bg7").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+
+
+		});
